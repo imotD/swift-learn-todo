908ef0241922cc4db9eca15f1ade95330d00ef72
Tommy Alhamra <tommy.alhamra@ist.id>
2025-03-25 11:10:47 +0700

    Initial Commit

---
+++ SwiftTodo/SwiftTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTodoApp.swift
+//  SwiftTodo
+//
+//  Created by Tommy Alhamra on 25/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftTodoTests/SwiftTodoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTodoTests.swift
+//  SwiftTodoTests
+//
+//  Created by Tommy Alhamra on 25/03/25.
+//
+
+import Testing
+@testable import SwiftTodo
+
+struct SwiftTodoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
