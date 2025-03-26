b9c98351a2b2f1eab38f45b8bece51be7598e09e
Tommy Alhamra <tommy.alhamra@ist.id>
2025-03-26 10:57:47 +0700

    learn layout vstack,zstack,hstack

--- SwiftTodo/ContentView.swift
+++ SwiftTodo/ContentView.swift
@@ -9,13 +9,73 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Image("bg")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing:20){
+                HandView()
+                FormBox()
+                    .padding()
+            }
+            .padding(.all, 20)
         }
-        .padding()
+    }
+}
+
+struct HandView: View {
+    var body: some View {
+        VStack{
+            Image("mcode")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .background(Color("CustomColor"))
+                .foregroundColor(Color.white)
+                .padding()
+                .background(Color("CustomColor"))
+                .cornerRadius(20)
+            
+            Text("Murid Coding")
+                .foregroundStyle(Color.white)
+        }
+    }
+}
+
+struct FormBox: View{
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    
+    var body: some View{
+        VStack(alignment: .leading) {
+            Text("Username").font(.callout).bold()
+            
+            TextField("Username", text: $username)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Text("Password").font(.callout).bold()
+            
+            SecureField("Password", text: $password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Button(action: {
+                print("Login Success")
+                print($username, $password)
+            }){
+                HStack{
+                    Text("Sign In")
+                    Spacer()
+                }
+            }
+            .padding()
+            .background(Color("CustomColor"))
+            .foregroundStyle(Color.white)
+            .cornerRadius(8)
+        }
+        .padding(.all, 30)
+        .background(Color.blue)
+        .cornerRadius(10)
     }
 }
 
